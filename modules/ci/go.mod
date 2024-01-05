d5e4acebd936b6311a98f9f4bc334eb59f9fa0ba
Matheus Andrade <matheus.add12@gmail.com>
2024-01-04 21:00:24 -0300

    ci: add github workflow ci

---
+++ modules/ci/go.mod
@@ -0,0 +1,3 @@
+module main_math
+
+go 1.21.4
---
+++ modules/ci/math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T){
+	total := sum(15, 15)
+	if total != 30 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 30)
+	}
+}
