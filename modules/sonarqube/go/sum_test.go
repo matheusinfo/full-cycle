62a34b88a6d5ba515f9ab8fe5af9170c783abcf6
Matheus Andrade <matheus.add12@gmail.com>
2024-01-06 15:37:46 -0300

    test: add tests to golang project

--- modules/sonarqube/go/sum_test.go
+++ modules/sonarqube/go/sum_test.go
@@ -9,3 +9,27 @@ func TestSum(t *testing.T){
 		t.Error("Expected 2 + 2 to equal 4")
 	}
 }
+
+func TestSub(t *testing.T){
+	result := sub(2, 2)
+	
+	if result != 0 {
+		t.Error("Expected 2 - 2 to equal 0")
+	}
+}
+
+func TestMul(t *testing.T){
+	result := mul(2, 2)
+	
+	if result != 4 {
+		t.Error("Expected 2 * 2 to equal 4")
+	}
+}
+
+func TestDiv(t *testing.T){
+	result := div(2, 2)
+	
+	if result != 1 {
+		t.Error("Expected 2 / 2 to equal 1")
+	}
+}
